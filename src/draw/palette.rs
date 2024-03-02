e3e5717a3a1773e16d735ba3ae52a9dd8d8d71ce
James McClung <james@themcclungs.net>
2024-03-02 17:36:15 -0500

    palette: show details

--- src/draw/palette.rs
+++ src/draw/palette.rs
@@ -16,21 +16,28 @@ impl Plugin for PalettePlugin {
 struct PaletteRoot;
 
 #[derive(Component)]
-struct PaletteText;
+struct PaletteTitle;
+
+#[derive(Component)]
+struct PaletteDetails;
 
 #[derive(Component)]
 pub struct ParticleToDraw(pub Particle);
 
 const INITIAL_PARTICLE_TO_DRAW: Particle = Particle::Air { gas_properties: GasProperties::DEFAULT };
 
+fn get_style() -> TextStyle {
+    TextStyle {
+        font_size: 16.0,
+        color: Color::WHITE,
+        ..default()
+    }
+}
+
 fn get_section(text: impl Into<String>) -> TextSection {
     TextSection {
         value: text.into(),
-        style: TextStyle {
-            font_size: 16.0,
-            color: Color::WHITE,
-            ..default()
-        },
+        style: get_style(),
     }
 }
 
@@ -43,13 +50,14 @@ fn setup_palette(mut commands: Commands) {
                 left: Val::Percent(75.0),
                 top: Val::Percent(40.0),
                 padding: UiRect::all(Val::Px(4.0)),
+                flex_direction: FlexDirection::Column,
                 ..default()
             },
             ..default()
         },
     )).with_children(|parent| {
         parent.spawn((
-            PaletteText,
+            PaletteTitle,
             TextBundle {
                 text: Text::from_sections([
                     get_section("Drawing: "),
@@ -62,9 +70,37 @@ fn setup_palette(mut commands: Commands) {
                 ..default()
             }
         ));
+        parent.spawn((
+            PaletteDetails,
+            TextBundle {
+                text: Text::from_section(get_details(&INITIAL_PARTICLE_TO_DRAW), get_style()),
+                ..default()
+            }
+        ));
     });
 }
 
 fn setup_particle_to_draw(mut commands: Commands) {
     commands.spawn(ParticleToDraw(INITIAL_PARTICLE_TO_DRAW));
+}
+
+fn get_details(particle: &Particle) -> String {
+    match particle {
+        Particle::Vacuum => "".into(),
+        Particle::Air { gas_properties } => gas_property_details(gas_properties),
+    }
+}
+
+fn gas_property_details(gas_properties: &GasProperties) -> String {
+    let mass = gas_properties.mass;
+    let velocity = gas_properties.velocity();
+    let vx = velocity.x;
+    let vy = velocity.y;
+    let temperature = gas_properties.temperature();
+    format!("\
+GAS PROPERTIES
+  mass:        {mass:5.1} kg
+  velocity:    ({vx:4.2}, {vy:4.2}) m/s
+  temperature: {temperature:5.1} K\
+")
 }
