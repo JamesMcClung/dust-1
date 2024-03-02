0d123fb09483400d50aeedd388af72e4334b97e6
James McClung <james@themcclungs.net>
2024-03-02 11:45:53 -0500

    schedule: add stepping

--- src/schedule.rs
+++ src/schedule.rs
@@ -12,6 +12,7 @@ pub enum SimSet {
 pub enum SimState {
     Playing,
     Paused,
+    Stepping,
 }
 
 pub struct SchedulePlugin;
@@ -20,18 +21,21 @@ impl Plugin for SchedulePlugin {
     fn build(&self, app: &mut App) {
         app
             .insert_state(SimState::Playing)
-            .add_systems(Update, toggle_pause)
+            .add_systems(Update, (
+                handle_state_inputs,
+                stop_stepping.run_if(in_state(SimState::Stepping)),
+            ))
             .configure_sets(
                 Update,
                 (SimSet::Gravity, SimSet::Gas, SimSet::Draw, SimSet::Recolor)
                     .chain()
-                    .run_if(in_state(SimState::Playing))
+                    .run_if(in_state(SimState::Playing).or_else(in_state(SimState::Stepping)))
             )
         ;
     }
 }
 
-fn toggle_pause(
+fn handle_state_inputs(
     mut next_state: ResMut<NextState<SimState>>,
     state: Res<State<SimState>>,
     inputs: Res<ButtonInput<KeyCode>>,
@@ -40,6 +44,18 @@ fn toggle_pause(
         match state.get() {
             SimState::Paused => next_state.set(SimState::Playing),
             SimState::Playing => next_state.set(SimState::Paused),
+            _ => (),
+        }
+    } else if inputs.just_pressed(KeyCode::Period) {
+        match state.get() {
+            SimState::Paused => next_state.set(SimState::Stepping),
+            _ => ()
         }
     }
+}
+
+fn stop_stepping(
+    mut next_state: ResMut<NextState<SimState>>,
+) {
+    next_state.set(SimState::Paused)
 }
