bc1bae61b07a109d2d2e65fa593b0ed6e2230432
James McClung <james@themcclungs.net>
2024-03-02 11:37:51 -0500

    schedule: add pausing

--- src/schedule.rs
+++ src/schedule.rs
@@ -8,10 +8,38 @@ pub enum SimSet {
     Recolor,
 }
 
+#[derive(States, Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub enum SimState {
+    Playing,
+    Paused,
+}
+
 pub struct SchedulePlugin;
 
 impl Plugin for SchedulePlugin {
     fn build(&self, app: &mut App) {
-        app.configure_sets(Update, (SimSet::Gravity, SimSet::Gas, SimSet::Draw, SimSet::Recolor).chain());
+        app
+            .insert_state(SimState::Playing)
+            .add_systems(Update, toggle_pause)
+            .configure_sets(
+                Update,
+                (SimSet::Gravity, SimSet::Gas, SimSet::Draw, SimSet::Recolor)
+                    .chain()
+                    .run_if(in_state(SimState::Playing))
+            )
+        ;
+    }
+}
+
+fn toggle_pause(
+    mut next_state: ResMut<NextState<SimState>>,
+    state: Res<State<SimState>>,
+    inputs: Res<ButtonInput<KeyCode>>,
+) {
+    if inputs.just_pressed(KeyCode::Space) {
+        match state.get() {
+            SimState::Paused => next_state.set(SimState::Playing),
+            SimState::Playing => next_state.set(SimState::Paused),
+        }
     }
 }
